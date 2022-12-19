f7c04e45de9bd3d83c68bc05955ed563ed8874b7
kannan-ravi <kannanravindran00kr@gmail.com>
2022-12-19 20:47:52 +0530

    Added strictQuery to false, to keep hiding the warning thrown by mongoose

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
+    mongoose.set('strictQuery', false);
     await mongoose.connect(process.env.DATABASE_URI, {
       dbName: "uploadImage",
       useUnifiedTopology: true,
